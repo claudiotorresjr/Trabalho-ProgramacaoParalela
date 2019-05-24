5b468d9d43a374e84e0cb55fcad35a4d205a9f8e
ctj17 <ctj17@inf.ufpr.br>
2019-05-24 03:25:53 -0300

    inicio

---
+++ confere.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#pograma a ser rodado recebe o primeiro argumento do script
+programa="$1"
+
+#tamanho inicial e final da matriz, sendo que de inicio a final vai incrementando em base 2
+inicio=8
+fim=16
+#passo e o multiplicador para os numeros ficarem na base 2
+passo=2
+
+resultados="resultadosBase"
+#se nao existe o diretorio "resultadosBase", crio um e calculo varios resultados
+#de diversos sistemas lineares de "inicio" ate "fim" tamanho
+if [[ ! -e "$resultados" ]] 
+then
+    mkdir "$resultados"
+
+    while [ "$inicio" -le "$fim" ]
+	do
+		./sisLinear -n "$inicio" > "$resultados"/"$inicio".txt
+
+		(( inicio = inicio*"$passo" ))
+	done
+fi
+
+#executa outra versao do programa e verifica se o resultado eh igual ao da versao base
+while [ "$inicio" -le "$fim" ]
+do
+	./"$programa" -n "$inicio" > "$inicio".temp
+
+	if diff "$resultados"/"$inicio".txt "$inicio".temp
+	then
+		echo "certo"
+	else
+		echo "Deu ruim"
+    	exit 1
+    fi
+
+	(( inicio = inicio*"$passo" ))
+done
+
+rm *.temp
